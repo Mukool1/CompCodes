2a9ec854ea222c5ea4c4a5bc6ad6ff45b33e7e27
Mukool1 <workwithmukool@gmail.com>
2025-12-12 02:01:49 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using  namespace std; int main(){    int a,b;    cin>>a;    cin>>b;    int count=0;    while(a<=b){        a*=3;        b*=2;        count++;    }    cout<<count<<endl;    return 0;}
