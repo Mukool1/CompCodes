8508728b7f0ae7946e4878b6722c35e5a5c6082e
Mukool1 <workwithmukool@gmail.com>
2026-01-01 17:51:29 +0530

    Time: 0 ms (100%), Space: 11.4 MB (96.83%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int countOfzero=0;
-        int countOfone=0;
-        int countOftwo=0;
-        for(int i=0;i<nums.size();i++){
-            switch(nums[i]){
-            case 0 : countOfzero++;
-            break;
-            case 1:countOfone++;
-            break;
-            case 2:countOftwo++;
-            break;
+        int low=0,mid=0,high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                mid++;
+                low++;
             }
-        }
-        int index=0;
-        for(int i=0;i<countOfzero;i++){
-            nums[index++]=0;
-        }
-        for(int i=0;i<countOfone;i++){
-            nums[index++]=1;
-        }
-        for(int i=0;i<countOftwo;i++){
-            nums[index++]=2;
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
         }
     }
 };
