202bbf456b504f1c3074e58ffde6ef0d3a86b33f
Mukool1 <workwithmukool@gmail.com>
2026-02-16 15:08:51 +0530

    Time: 31 ms (86.52%), Space: 53.4 MB (83.72%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPal(string s){
+        int j=s.size()-1;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            else{
+                j--;
+            }
+        }
+        return true;
+    }
+    void getPart(string s,vector<string>&partitions,vector<vector<string>>&ans){
+        if(s.size()==0){
+            ans.push_back(partitions);
+            return;
+        }
+        for(int i=0;i<s.size();i++){
+            string part=s.substr(0,i+1);
+            if(isPal(part)){
+                partitions.push_back(part);
+                getPart(s.substr(i+1),partitions,ans);
+                partitions.pop_back();
+            }
+        }
+    }
+
+    vector<vector<string>> partition(string s) {
+        vector<string>partitions;vector<vector<string>>ans;
+        getPart(s,partitions,ans);
+        return ans;
+    }
+};
