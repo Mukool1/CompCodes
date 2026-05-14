96a43ce2f50eef1eba23a82410b0e9bf3a57befd
Mukool1 <workwithmukool@gmail.com>
2026-05-15 00:36:31 +0530

    Time: 31 ms (51.66%), Space: 19.3 MB (46.11%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -32,6 +32,18 @@ public:
         dp[amount]=mini;
         return mini;
     }
+//tabulation
+    int solveTab(vector<int>& coins, int amount){
+        vector<int>dp(amount+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<coins.size();j++){
+                if(i-coins[j]>=0 && dp[i-coins[j]]!=INT_MAX)
+                dp[i]=min(dp[i],1+dp[i-coins[j]]);
+            }
+        }
+        return dp[amount];
+    }
     int coinChange(vector<int>& coins, int amount) {
         // int ans=solve(coins, amount);
         // if(ans!=INT_MAX)return ans;
