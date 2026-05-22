c03fafae479f24c94e4dfe3fdd56379ec520a648
Mukool1 <workwithmukool@gmail.com>
2026-05-22 11:19:46 +0530

    Add Construct an Array [A] from Codeforces

---
+++ 2231/A - Construct an Array/solution.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+ 
+void solve()
+{
+  int n;
+  cin >> n;
+ 
+  for (int i = 0; i < n; i++)
+  {
+    cout << 2 * i + 1 << " ";
+  }
+  cout << "
+";
+}
+ 
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+  return 0;
+}
