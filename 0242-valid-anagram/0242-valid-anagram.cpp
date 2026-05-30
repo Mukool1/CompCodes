de0485852b2683d6172ed021f940f809bbf08c24
Mukool1 <workwithmukool@gmail.com>
2026-05-30 09:31:36 +0530

    Time: 3 ms (45.36%), Space: 9.6 MB (75.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,12 +4,23 @@ public:
         if(s.size()!=t.size()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        for(int i=0;i<s.size();i++){
-            if(s[i]!=t[i]){
-                return false;
-            }
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]!=t[i]){
+        //         return false;
+        //     }
+        // }
+        // return true;
+        vector<int>freq(26,0);
+        for(char c : s){
+            freq[c-'a']++;
+        }
+        for(char c : t){
+            freq[c-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0)return false;
         }
         return true;
     }
