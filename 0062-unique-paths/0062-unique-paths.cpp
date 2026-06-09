55aaeb6dbdfc5ddad41df6af8de33723b7497111
Mukool1 <workwithmukool@gmail.com>
2026-06-09 22:22:45 +0530

    Time: 0 ms (100%), Space: 9.3 MB (51.32%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,18 +1,44 @@
 class Solution {
 public:
-int memo[101][101];
-    int solve(int currRow,int currCol){
-        if(currRow==0 && currCol==0){
+    int memo[101][101];
+    // recursion
+    int solveRec(int currRow, int currCol) {
+
+        if (currRow == 0 && currCol == 0) {
             return 1;
         }
-        if(currRow<0 || currCol<0){
+        if (currRow < 0 || currCol < 0) {
             return 0;
         }
-        if(memo[currRow][currCol]!=-1)return memo[currRow][currCol];
-        return memo[currRow][currCol]=solve(currRow-1,currCol)+solve(currRow,currCol-1);
+        return solveRec(currRow - 1, currCol) + solveRec(currRow, currCol - 1);
+    }
+    // memoisation
+    int solveMemo(int currRow, int currCol) {
+        if (currRow == 0 && currCol == 0) {
+            return 1;
+        }
+        if (currRow < 0 || currCol < 0) {
+            return 0;
+        }
+        if (memo[currRow][currCol] != -1)
+            return memo[currRow][currCol];
+        return memo[currRow][currCol] = solveMemo(currRow - 1, currCol) +
+                                        solveMemo(currRow, currCol - 1);
+    }
+    // tabulation
+    int solveTab(int currRow, int currCol) {
+        vector < vector < int >> dp(currRow, vector<int>(currCol, 1));
+        for (int i = 1; i < currRow; i++) {
+            for (int j = 1; j < currCol; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[currRow-1][currCol-1];
     }
     int uniquePaths(int m, int n) {
         memset(memo, -1, sizeof(memo));
-        return solve(m-1,n-1);
+        // return solveRec(m - 1, n - 1);
+        // return solveMemo(m-1,n-1);
+        return solveTab(m , n );
     }
 };
