430569d82a07dcd907570150f14e719787d47bb1
Mukool1 <workwithmukool@gmail.com>
2026-06-10 19:25:12 +0530

    Time: 0 ms (100%), Space: 16.3 MB (58.78%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    //recursion
-    int solveRec(vector<vector<int>>& grid,int m,int n){
-        if(m>=grid.size() || n>=grid[0].size())return INT_MAX;
-        if(m==grid.size()-1 && n==grid[0].size()-1)return grid[m][n];
-        return grid[m][n]+min(solveRec(grid,m+1,n),solveRec(grid,m,n+1));
+    // recursion
+    int solveRec(vector<vector<int>>& grid, int m, int n) {
+        if (m >= grid.size() || n >= grid[0].size())
+            return INT_MAX;
+        if (m == grid.size() - 1 && n == grid[0].size() - 1)
+            return grid[m][n];
+        return grid[m][n] +
+               min(solveRec(grid, m + 1, n), solveRec(grid, m, n + 1));
     }
-    //memoisation
-    int solveMemo(vector<vector<int>>& grid,int m,int n,vector<vector<int>>&memo){
-        if(m>=grid.size() || n>=grid[0].size())return INT_MAX;
-        if(m==grid.size()-1 && n==grid[0].size()-1)return grid[m][n];
-        if(memo[m][n]!=-1)return memo[m][n];
-        return memo[m][n]=grid[m][n]+min(solveMemo(grid,m+1,n,memo),solveMemo(grid,m,n+1,memo));
+    // memoisation
+    int solveMemo(vector<vector<int>>& grid, int m, int n,
+                  vector<vector<int>>& memo) {
+        if (m >= grid.size() || n >= grid[0].size())
+            return INT_MAX;
+        if (m == grid.size() - 1 && n == grid[0].size() - 1)
+            return grid[m][n];
+        if (memo[m][n] != -1)
+            return memo[m][n];
+        return memo[m][n] = grid[m][n] + min(solveMemo(grid, m + 1, n, memo),
+                                             solveMemo(grid, m, n + 1, memo));
+    }
+    // tabulation
+    int solveTab(vector<vector<int>>& grid, int m, int n) {
+        if (grid.empty())
+            return 0;
+        vector<vector<int>> dp(grid.size(), vector<int>(grid[0].size(), 0));
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < grid.size(); i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < grid[0].size(); j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < grid.size(); i++) {
+            for (int j = 1; j < grid[0].size(); j++) {
+                dp[i][j] += grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m - 1][n - 1];
     }
     int minPathSum(vector<vector<int>>& grid) {
-        vector<vector<int>>memo(grid.size(),vector<int>(grid[0].size(),-1));
+        // vector<vector<int>>memo(grid.size(),vector<int>(grid[0].size(),-1));
         // return solveRec(grid,0,0);
-        return solveMemo(grid,0,0,memo);
+        // return solveMemo(grid,0,0,memo);
+        return solveTab(grid, grid.size(), grid[0].size());
     }
 };
