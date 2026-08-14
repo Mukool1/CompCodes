02c6c94fc5646a382a5abffcfb39e54b7ddee148
Mukool1 <workwithmukool@gmail.com>
2026-08-14 09:53:02 +0530

    Time: 2 ms (36.62%), Space: 11 MB (25.63%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //backtracking
     void printSubsets(vector<int>nums,vector<vector<int>>&ans,vector<int>&curr,int i){
         if(i==nums.size()){
             ans.push_back(curr);return;
@@ -9,10 +10,27 @@ public:
         curr.pop_back();
         printSubsets(nums,ans,curr,i+1);
     }
-    vector<vector<int>> subsets(vector<int>& nums) {
+    //i/p o/p
+    void ipop(vector<int>&nums,vector<vector<int>>&ans,vector<int>&curr){
+        ans.push_back(curr);
+        if(nums.size()==0)return;
+        for(int i=0;i<nums.size();i++){
+            vector<int>ip(nums),op(curr);
+            op.push_back(nums[i]);
+
+            for(int j=0;j<=i;j++){
+                ip.erase(ip.begin());
+            }
+            ipop(ip,ans,op);
+        }
+
+    }
+    vector<vector<int>> subsets(vector<int>& nums) { 
         vector<vector<int>>ans;
         vector<int>curr;
-        printSubsets(nums,ans,curr,0);
+        // printSubsets(nums,ans,curr,0);
+        ipop(nums,ans,curr);
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
