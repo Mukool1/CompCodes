0886f3035350540bcdfe63b9bc02acb10ac22a91
Mukool1 <workwithmukool@gmail.com>
2026-08-19 15:10:43 +0530

    Time: 3 ms (31.25%), Space: 11.4 MB (16.33%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -39,17 +39,32 @@ public:
             ipop_prune(ip,st,op);
         }
     }
+    void pickNotPick(vector<int>& nums,vector<vector<int>>&ans,vector<int>&curr){
+        ans.push_back(curr);
+        if(nums.size()==0)return;
+        for(int i=0;i<nums.size();i++){
+            vector<int>ip(nums),op(curr);
+            if(i>0 && nums[i]==nums[i-1])continue;
+            op.push_back(nums[i]);
+            for(int j=0;j<=i;j++){
+                ip.erase(ip.begin());
+            }
+            pickNotPick(ip,ans,op);
+        }
+    }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>curr;
         // sub(nums,ans,curr,0);
         // ipop(nums,ans,curr);
-        set<vector<int>>st;
-        ipop_prune(nums,st,curr);
+        // set<vector<int>>st;
+        // ipop_prune(nums,st,curr);
         // set<vector<int>> unique_subsets(ans.begin(), ans.end());
         // return vector<vector<int>>(unique_subsets.begin(), unique_subsets.end());
-        return vector<vector<int>>(st.begin(), st.end());
+        // return vector<vector<int>>(st.begin(), st.end());
+        pickNotPick(nums,ans,curr);
+        return ans;
         // return ans;
     }
 };
