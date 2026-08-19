6b230c0c84fac8ef63cd9f74098ff6c3feb03c70
Mukool1 <workwithmukool@gmail.com>
2026-08-19 13:51:26 +0530

    Time: 3 ms (31.25%), Space: 12.9 MB (14.37%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,22 +1,39 @@
 class Solution {
 public:
+//recursion
     void sub(vector<int> nums,vector<vector<int>>&ans,vector<int>&curr,int i){
         if(i==nums.size()){
-            ans.push_back({curr});
+            ans.push_back(curr);
             return;
         }
         curr.push_back(nums[i]);
         sub(nums,ans,curr,i+1);
         curr.pop_back();
-        int indx=i+1;
-        while(indx<nums.size()&&nums[indx]==nums[indx-1])indx++;
-        sub(nums,ans,curr,indx);
+        int index=i+1;
+        while(index<nums.size() && nums[index-1]==nums[index])index++;
+        sub(nums,ans,curr,index);
+    }
+//ip/op
+    void ipop(vector<int>& nums,vector<vector<int>>&ans,vector<int>&curr){
+        ans.push_back(curr);
+        if(nums.size()==0)return;
+        for(int i=0;i<nums.size();i++){
+            vector<int>ip(nums),op(curr);
+            op.push_back(nums[i]);
+            for(int j=0;j<=i;j++){
+                ip.erase(ip.begin());
+            }
+            ipop(ip,ans,op);
+        }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
         vector<int>curr;
-        sub(nums,ans,curr,0);
-        return ans;
+        // sub(nums,ans,curr,0);
+        ipop(nums,ans,curr);
+        set<vector<int>> unique_subsets(ans.begin(), ans.end());
+        return vector<vector<int>>(unique_subsets.begin(), unique_subsets.end());
+        // return ans;
     }
 };
