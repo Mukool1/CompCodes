73f90f8e0aa95f7c2002631e9f71cde6e47b81ca
Mukool1 <workwithmukool@gmail.com>
2026-08-21 10:33:00 +0530

    Time: 19 ms (17.05%), Space: 16.4 MB (6.25%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -11,11 +11,13 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             vector<int> ip(nums), op(curr);
             op.push_back(nums[i]);
+            if(i>0 && nums[i]==nums[i-1])continue;
             ip.erase(ip.begin()+i);
             ipop(ip, op,ans,mp);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
         vector<int>curr;
         vector<vector<int>>ans;
         map<vector<int>,int>mp;
