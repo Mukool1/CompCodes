1650fe09cb227c17659f8509a635bbe13aaaff84
Mukool1 <workwithmukool@gmail.com>
2026-08-21 10:22:25 +0530

    Time: 354 ms (5.09%), Space: 109.3 MB (5.04%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<vector<int>>&ans,map<vector<int>,int>&mp,int indx){
-        if(indx==nums.size()){
-            if(!mp.count(nums)){
-                ans.push_back(nums);
-                mp[nums]++;
-                
+    void ipop(vector<int>& nums, vector<int>& curr, vector<vector<int>>& ans,map<vector<int>,int>&mp) {
+        if (nums.size() == 0) {
+            if(!mp.count(curr)){
+            ans.push_back(curr);
+            mp[curr]++;
             }
             return;
         }
-        for(int i=indx;i<nums.size();i++){
-            swap(nums[i],nums[indx]);
-            solve(nums,ans,mp,indx+1);
-            swap(nums[i],nums[indx]);
+        for (int i = 0; i < nums.size(); i++) {
+            vector<int> ip(nums), op(curr);
+            op.push_back(nums[i]);
+            ip.erase(ip.begin()+i);
+            ipop(ip, op,ans,mp);
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        // vector<int>curr;
+        vector<int>curr;
         vector<vector<int>>ans;
         map<vector<int>,int>mp;
         
-        solve(nums,ans,mp,0);
+        ipop(nums,curr,ans,mp);
         return ans;
     }
 };
