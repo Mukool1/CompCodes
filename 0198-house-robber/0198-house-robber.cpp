8ecca0b0fc63611fa705e23c3895b980ed060fcd
Mukool1 <workwithmukool@gmail.com>
2026-08-27 09:43:22 +0530

    Time: 0 ms (100%), Space: 10.8 MB (6.15%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,14 +1,31 @@
+
 class Solution {
 public:
+    int rec(int i,vector<int>&nums){
+        if(i<0)return 0;
+        if(i==0)return nums[0];
+        int rob=nums[i]+rec(i-2,nums);
+        int skip=rec(i-1,nums);
+        return max(rob,skip);
+    }
+    int memo(vector<int>&dp,int i,vector<int>&nums){
+        if(i<0)return 0;
+        if(i==0)return nums[0];
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=max(nums[i]+memo(dp,i-2,nums),memo(dp,i-1,nums));
+    }
     int rob(vector<int>& nums) {
-        if(nums.size()<=1)return nums[0];
-        vector<int>dp(nums.size()+1);
-        dp[0]=nums[0];
-        dp[1]=max(nums[1],nums[0]);
-        for(int i=2;i<nums.size();i++){
-            // maxRob=max(robCurr,robPrev+nums[i]);
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
-        }
-        return dp[nums.size()-1];
+        vector<int>dp(nums.size(),-1);
+        // if(nums.size()<=1)return nums[0];
+        // vector<int>dp(nums.size()+1);
+        // dp[0]=nums[0];
+        // dp[1]=max(nums[1],nums[0]);
+        // for(int i=2;i<nums.size();i++){
+        //     // maxRob=max(robCurr,robPrev+nums[i]);
+        //     dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        // }
+        // return dp[nums.size()-1];
+        // return rec(nums.size()-1,nums);
+        return memo(dp,nums.size()-1,nums);
     }
 };
